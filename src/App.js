4e516af6508178e1e743e9ce71ccb52344fc952a
Thein Naing <theinnaing0908@gmail.com>
2023-05-18 10:03:06 +0800

    create people and index useState hooks for impementing fetching person data from data.js and in return section destructure person data with const {} = person object

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+
+
+function App() {
+  const[people, setPeople] = useState(data);
+  const[index, setIndex]  = useState(0);
+
+
+  return (
+    <section>
+      <div className='section'>
+        <h3>
+          <span>
+            /
+          </span>
+          reviews
+        </h3>
+        </div>
+        <div className='section-center'>
+          {people.map((person, personIndex)=> {
+
+                    const {id, image, name, title, quote} = person;
+                    return (
+                      <article>
+
+                      </article>
+
+                    )
+
+          })}
+      </div>
+    </section>
+  )
+}
+
+export default App;
